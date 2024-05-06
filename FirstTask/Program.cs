685932f816a4114731cea2c88bc290d0cd0d0b45
Cavid112233 <suleymanli535@gmail.com>
2024-05-06 23:30:09 +0400

    first

--- FirstTask/Program.cs
+++ FirstTask/Program.cs
@@ -5,10 +5,11 @@ namespace FirstTask
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
+            app.UseStaticFiles();
+            app.MapControllerRoute("default","{controller=home}/{action=index}/{id?}");
+            app.UseStaticFiles();
             app.Run();
         }
     }
